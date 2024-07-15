4fcfdfa8f9b35c79837f5ce51b10e708c50ae87d
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-07-15 15:29:11 -0300

    brands create view, form, template and url

--- brands/urls.py
+++ brands/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('brands/list/', views.BrandListView.as_view(), name='brand_list'),
+    path('brands/create/', views.BrandCreateView.as_view(), name='brand_create'),
 ]
--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
-from . import models
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from . import models, forms
 
 
 class BrandListView(ListView):
@@ -15,3 +16,10 @@ class BrandListView(ListView):
             queryset = queryset.filter(name__icontains=name)
 
         return queryset
+
+
+class BrandCreateView(CreateView):
+    model = models.Brand
+    template_name = 'brand_create.html'
+    form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
