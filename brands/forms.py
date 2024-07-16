05bce3174429a405b49d402de6d5d4ac22fe14db
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-07-16 15:09:03 -0300

    brand form widgets and labels

--- brands/forms.py
+++ brands/forms.py
@@ -7,3 +7,11 @@ class BrandForm(forms.ModelForm):
     class Meta:
         model = models.Brand
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+        labels = {
+            'name': 'Nome',
+            'description': 'Descrição',
+        }
