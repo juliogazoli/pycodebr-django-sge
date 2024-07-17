effbb491f7c515ae7b3eded04be991c96ddd03f5
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-07-17 15:11:44 -0300

    brands delete view, template and url

--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from . import models, forms
 
 
@@ -34,4 +34,10 @@ class BrandUpdateView(UpdateView):
     model = models.Brand
     template_name = 'brand_update.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
