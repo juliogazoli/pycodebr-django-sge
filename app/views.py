32e469bd7fd6878848af8c9f1095cefed1ebfeaa
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-07-29 15:32:24 -0300

    product metrics on context

--- app/views.py
+++ app/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {}
+    product_metrics = {
+        'total_quantity': 1000,
+        'total_cost_price': 100000,
+        'total_selling_price': 300000,
+        'total_profit': 200000,
+    }
+
+    context = {
+        'product_metrics': product_metrics
+    }
+
     return render (request, 'home.html', context)
