4b2c3278d7c8c2431f3d5369cfdd02030bb9b024
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-08-02 15:18:51 -0300

    daily sales metrics

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from . import metrics
 
@@ -5,10 +6,12 @@ from . import metrics
 def home(request):   
     product_metrics = metrics.get_product_metrics()
     sales_metrics = metrics.get_sales_metrics()
+    daily_sales_data = metrics.get_daily_sales_data()
 
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
+        'daily_sales_data': json.dumps(daily_sales_data),
     }
 
     return render (request, 'home.html', context)
