678feb65c2456c385f794de93756b834b3cdf055
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-08-02 16:03:48 -0300

    products graphics

--- app/metrics.py
+++ app/metrics.py
@@ -1,6 +1,8 @@
 from django.db.models import Sum, F
 from django.utils import timezone
 from django.utils.formats import number_format
+from brands.models import Brand
+from categories.models import Category
 from products.models import Product
 from outflows.models import Outflow
 
@@ -70,3 +72,13 @@ def get_daily_sales_quantity_data():
         dates=dates,
         values=quantities,
     )
+
+
+def get_graphic_product_category_metric():
+    categories = Category.objects.all()
+    return {category.name: Product.objects.filter(category=category).count() for category in categories}
+
+
+def get_graphic_product_brand_metric():
+    brands = Brand.objects.all()
+    return {brand.name: Product.objects.filter(brand=brand).count() for brand in brands}
