98f98631a14f4f3aa9851459448feb33f03436cd
Julio dos Santos Gazoli <julio.gazoli@prefeitura.local>
2024-08-02 15:32:32 -0300

    daily sales quantity metrics

--- app/views.py
+++ app/views.py
@@ -7,11 +7,13 @@ def home(request):
     product_metrics = metrics.get_product_metrics()
     sales_metrics = metrics.get_sales_metrics()
     daily_sales_data = metrics.get_daily_sales_data()
+    daily_sales_quantity_data = metrics.get_daily_sales_quantity_data()
 
     context = {
         'product_metrics': product_metrics,
         'sales_metrics': sales_metrics,
         'daily_sales_data': json.dumps(daily_sales_data),
+        'daily_sales_quantity_data': json.dumps(daily_sales_quantity_data)
     }
 
     return render (request, 'home.html', context)
